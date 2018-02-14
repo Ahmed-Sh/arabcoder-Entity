f754597b0d456003e49bac247ee428c286a7698e
Ahmed Abdou El-Tabai ElZahar <ahmed.elzahar@azmeel.com>
2018-02-14 10:51:49 +0300

    1st-modefy add member and post classes

---
+++ main.py
@@ -0,0 +1,9 @@
+import models
+member1=models.member("ahmed",32)
+member2=models.member("ali",25)
+post1=models.post("1st post","this is my 1st post")
+post2=models.post("2nd post","this is my 2nd post")
+post3=models.post("3rd post","this is my 3rd post")
+print member1.name
+print member2.age
+print post3.subject
---
+++ models.py
@@ -0,0 +1,12 @@
+class member():
+	"""docstring for ClassName"""
+	def __init__(self, name,age):
+		self.name = name
+		self.age=age
+class post():
+	def __init__(self, title,subject):
+		self.title = title
+		self.subject=subject
+		
+						
+		

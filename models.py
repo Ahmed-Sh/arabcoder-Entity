27e47d8fba44c50d015894c1f58500150a0be589
ahmed elzahar <ah_elzahar@yahoo.com>
2018-02-14 19:18:03 +0300

    add str to classes

--- models.py
+++ models.py
@@ -1,12 +1,17 @@
 class member():
 	"""docstring for ClassName"""
-	def __init__(self, name,age):
+	def __init__(self, name,age,id):
 		self.name = name
 		self.age=age
+		self.id=id
+	def __str__(self):
+		return "id: "+str(self.id) +"\nname:" +self.name+"\n=================="	
 class post():
 	def __init__(self, title,subject):
 		self.title = title
 		self.subject=subject
+	def __str__(self):
+		return "Title: "+self.title +"\n" +self.subject+"\n=================="
 		
 						
 		

11a27b70ea484f81f82932f7f48aad6384fbc1e0
ahmedelzahr <36417263+ahmedelzahr@users.noreply.github.com>
2018-02-21 16:43:08 +0300

    update store

--- store.py
+++ store.py
@@ -37,15 +37,49 @@ class MemberStore:
     if self.entity_exists(member): 
       MemberStore.members.remove(member)
 
-  def update(self,member,new_name,new_age):
-    member.name=new_name
-    member.age=new_age
+  def update(self, member):
+     result = member
+     all_members = self.get_all()
 
+     for index, current_member in enumerate(all_members):
+         if current_member.id == member.id:
+             all_members[index] = member
+             break
+
+     return result
   def get_by_name(self,name):
     
     for member in self.get_all():
       if member.name==name:
         yield member
+  def get_members_with_posts(self,all_post):
+    for post in all_post:
+      member_id=post.member_id
+      member=self.get_by_id(member_id)
+      if self.entity_exists(member):
+        member.posts.append(post)
+      self.update(member)  
+    return self.get_all()
+  def get_top_two(self,all_post):
+    first_member=0
+    second_member=0
+    member1=None
+    member2=None
+    top_two=[]
+    for member in self.get_all():
+      post_num= len(member.posts)
+      if post_num>=first_member:
+        member2=member1
+        second_member =first_member 
+        member1=member
+        first_member=post_num
+      elif post_num>=second_member:
+        member2=member
+        second_member =post_num     
+    top_two=[member1,member2]
+    return top_two
+
+          
 
 #-----------------------------------------------------------------------------------------------
 
@@ -61,5 +95,6 @@ class PostStore:
 
   def entity_exists(self, post): 
         return post in PostStore.posts
-           
 
+
+   
